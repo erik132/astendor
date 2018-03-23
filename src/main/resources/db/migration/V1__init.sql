dda6520433e9d5be27c82a4709080ffec622ed79
Erik Soekov <erik.soekov@gmail.com>
2018-03-23 23:16:39 +0200

    initial map in the database

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS maps;
+DROP TABLE IF EXISTS maptiles;
 
 CREATE TABLE maps (
   id INT(6)  AUTO_INCREMENT PRIMARY KEY,
@@ -6,5 +7,24 @@ CREATE TABLE maps (
   description VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE maptiles(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  map_id INT(6) NOT NULL,
+  x INT(4) NOT NULL,
+  y INT(4) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
 INSERT INTO maps (name, description) VALUES
   ("Moria", "Initial 3x3 test map");
+
+INSERT INTO maptiles(map_id, x, y, name) VALUES
+  (1,0,0, "Chaos mountain 1"),
+  (1,1,0, "chaos mountain 2"),
+  (1,2,0, "Mount Doom"),
+  (1,0,1, "Plains 1"),
+  (1,1,1, "chaos mountains 3"),
+  (1,2,1, "Forest 1"),
+  (1,0,2, "Kharakum Desert"),
+  (1,1,2, "Plains 2"),
+  (1,2,2, "Elvin Forest");
