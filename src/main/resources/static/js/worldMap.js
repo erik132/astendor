06f19f75df942dd456b8bce8b15a36f3716d38c6
erik132 <erik.soekov@gmail.com>
2018-03-30 19:05:04 +0300

    tooltip showing mechanic. Just needs styling

--- src/main/resources/static/js/worldMap.js
+++ src/main/resources/static/js/worldMap.js
@@ -4,3 +4,6 @@ function setEqual(){
         'height': cw + 'px'
     });
 }
+
+
+
---
+++ src/main/resources/static/js/worldMapBottom.js
@@ -0,0 +1,12 @@
+setEqual();
+
+var tooltips = document.querySelectorAll('.maptip .maptiptext');
+
+window.onmousemove = function (e) {
+    var x = (e.clientX + 20) + 'px',
+        y = (e.clientY + 20) + 'px';
+    for (var i = 0; i < tooltips.length; i++) {
+        tooltips[i].style.top = y;
+        tooltips[i].style.left = x;
+    }
+};
