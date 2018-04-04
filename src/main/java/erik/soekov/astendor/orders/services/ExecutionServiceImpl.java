174b9555563c9d069e073fdfee557abe4e5f0159
erik132 <erik.soekov@gmail.com>
2018-04-04 15:36:12 +0300

    commiting a bug: movement order not found

--- src/main/java/erik/soekov/astendor/orders/services/ExecutionServiceImpl.java
+++ src/main/java/erik/soekov/astendor/orders/services/ExecutionServiceImpl.java
@@ -1,6 +1,7 @@
 package erik.soekov.astendor.orders.services;
 
 import erik.soekov.astendor.orders.models.Order;
+import erik.soekov.astendor.orders.orderTypes.OrderFrame;
 import erik.soekov.astendor.orders.repos.OrderRepository;
 import erik.soekov.astendor.warlords.model.Warlord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,15 @@ public class ExecutionServiceImpl implements ExecutionService{
     public void executeWarlordOrders(Warlord warlord) {
         List<Order> orders = this.orderRepository.getWarlordOrders(warlord);
         System.out.println("nr of orders: " + orders.size());
+        for(Order order: orders) {
+            try {
+                OrderFrame frame = (OrderFrame) Class.forName(order.getOrderType().getOrderClass()).newInstance();
+                frame.setParams("params1");
+                frame.executeOrder();
+            } catch (Exception e) {
+                //just skip the order
+                System.out.println("Order not found " + order.getOrderType().getOrderClass());
+            }
+        }
     }
 }
